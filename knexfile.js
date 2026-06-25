d90239b02120945c882775c3993601af2d5b02e8
Hypocrisy <shinta.naga@icloud.com>
2026-06-25 07:03:51 +0000

    knex.jsを利用したデータベース操作に変更しよう

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,3 @@
-// Update with your config settings.
-const environment = "development";
-const config = require("../knexfile.js")[environment];
-const knex = require("knex")(config);
-
-module.exports = knex;
-
 module.exports = {
 
   development: {
