d118709628df105139b05b3daa1a099d60264538
Hypocrisy <shinta.naga@icloud.com>
2026-06-25 06:11:11 +0000

    mysqlモジュールを用いてデータベースに接続できるようにしよう

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql');
 
 let todos = [];
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'todo_app'
+});
 
 router.get('/', function (req, res, next) {
   res.render('index', {
@@ -11,8 +19,14 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
+  connection.connect((err) => {
+    if (err) {
+      console.log('error connecting: ' + err.stack);
+      return
+    }
+    console.log('success');
+  });
   const todo = req.body.add;
-  todos.push(todo);
   res.redirect('/');
 });
 
