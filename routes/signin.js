9e20cd8f47c9d4664d685c004192c19405480306
Hypocrisy <shinta.naga@icloud.com>
2026-07-09 06:22:48 +0000

    パスワードの照合をハッシュ化に対応させよう

--- routes/signin.js
+++ routes/signin.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const knex = require("../db/knex");
+const bcrypt = require("bcrypt");
 
 router.get('/', function (req, res, next) {
   const userId = req.session.userid;
@@ -18,23 +19,28 @@ router.post('/', function (req, res, next) {
   const password = req.body.password;
 
   knex("users")
-    .where({
-      name: username,
-      password: password,
-    })
+  .where({
+   name: username,
+  })
     .select("*")
-    .then((results) => {
-      if (results.length === 0) {
-        res.render("signin", {
-          title: "Sign in",
-          errorMessage: ["ユーザが見つかりません"],
-          isAuth: isAuth,
-        });
-      } else {
-        req.session.userid = results[0].id;
+    .then(async function (results) {
+    if (results.length === 0) {
+     res.render("signin", {
+      title: "Sign in",
+      errorMessage: ["ユーザが見つかりません"],
+      isAuth: isAuth,
+     });
+     } else if (await bcrypt.compare(password, results[0].password)) {
+         req.session.userid = results[0].id;
         res.redirect('/');
-      }
-    })
+      } else {
+         res.render("signin", {
+         title: "Sign in",
+          errorMessage: ["ユーザが見つかりません"],
+         isAuth: isAuth,
+      });
+    }
+  })
     .catch(function (err) {
       console.error(err);
       res.render("signin", {
